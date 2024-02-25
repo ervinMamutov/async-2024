7fc5056b0f6bacc82eacc580e2994381fa554558
Ervin Mamutov <ervin.news@gmail.com>
2024-02-25 08:57:20 +0100

    callback

--- callback/1-callback.js
+++ callback/1-callback.js
@@ -3,5 +3,12 @@
 // function
 const add = (a, b) => a + b;
 
-const res = (a, b);
-console.log(`add(a, b) = ${res}`);
+const res = add(2, 4);
+console.log(`add(2, 4) = ${res}`);
+
+// callback
+const sum = (a, b, callback) => callback(a + b);
+
+sum(2, 7, (res) => {
+  console.log(`sum(2, 7) = ${res}`);
+});
